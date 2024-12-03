1a72491117bc98f92466ead35ba733d545d9bcff
“muhammad <muhammadsamik681@gmail.com>
2024-12-04 01:37:23 +0500

    origin main

---
+++ app.js
@@ -0,0 +1,34 @@
+
+//question1
+var  user = prompt("enter your city")
+if(user === "karachi"){
+    alert("city of light")
+}
+//question 2
+var userGender = prompt("enter your gender")
+if(userGender === "male"){
+    console.log("welcome to sir");
+    
+}else{
+    console.log("welcome to madam");
+    
+}
+//question 3
+var testColor = prompt("enter your color")
+if (testColor === "red") {
+    console.log("mustStop");
+    
+}else if(testColor === "yellow"){
+console.log("Ready to Move");
+
+}else if(testColor === "blue"){
+    console.log("move now");
+    
+}
+//question 4
+var userInputFuel =prompt("enter your car fuel")
+var  fuel = 1.25
+if(userInputFuel < fuel){
+console.log("please fill Up your petrol");
+
+}
